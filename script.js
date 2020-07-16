0551d5c60484cbdfe53248444d2e41691a4e85bf
Abdullah-ohida <ohida2001@gmail.com>
2020-07-16 22:44:55 +0100

    Change erorr

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ navToggle.addEventListener('click', function(){
 const fullDate = document.querySelectorAll('.date-time');
 fullDate.forEach(function(items){
     items.innerHTML = new Date().toDateString();
+    console.log('helo world')
 })
 
 const date = document.querySelector(".date");
@@ -68,35 +69,56 @@ const loadItem = [
     const loadBtn = document.querySelector('.load-more');
     const sectionCenter = document.querySelector('.contain');
 
-    window.addEventListener('DOMContentLoaded', function(){
-        let displayItem = loadItem.map(function (item){
-            return ` <div class="benefit-container">
-            <div class="img-item">
-            <img src="${item.img}" alt="area">
-            </div>
-            <article>
-            <h2>${item.title}</h2>
-            <p>Date of Placement: <span class="date-time"></span></p>
-            <p class="location">Location: ${item.country}</p>
-            <p class="sponsored"> Sponsored by : ${item.sponsored}</p>
-            <button class="like" type="button">
-                <img src="/asset/imgs/heart.svg" alt="" class="heart">
-                <p class="text">50</p>
-            </button>
-            </artile>
-            </div>`
-        });
-        displayItem = displayItem.join("");
+    // window.addEventListener('DOMContentLoaded', function(){
+    //     let displayItem = loadItem.map(function (item){
+    //         return ` <div class="benefit-container">
+    //         <div class="img-item">
+    //         <img src="${item.img}" alt="area">
+    //         </div>
+    //         <article>
+    //         <h2>${item.title}</h2>
+    //         <p>Date of Placement: <span class="date-time"></span></p>
+    //         <p class="location">Location: ${item.country}</p>
+    //         <p class="sponsored"> Sponsored by : ${item.sponsored}</p>
+    //         <button class="like" type="button">
+    //             <img src="/asset/imgs/heart.svg" alt="" class="heart">
+    //             <p class="text">50</p>
+    //         </button>
+    //         </artile>
+    //         </div>`
+    //     });
+    //     displayItem = displayItem.join("");
 
 
-        sectionCenter.innerHTML = displayItem;
-    });
+    //     sectionCenter.innerHTML = displayItem;
+    // });
     loadBtn.addEventListener('click', function(){
         if(sectionCenter.classList.contains('show-more')){
             sectionCenter.classList.remove('show-more')
         }
         else{
-            sectionCenter.classList.add('show-more')
+            sectionCenter.classList.add('show-more');
+            let displayItem = loadItem.map(function (item){
+                return ` <div class="benefit-container">
+                <div class="img-item">
+                <img src="${item.img}" alt="area">
+                </div>
+                <article>
+                <h2>${item.title}</h2>
+                <p>Date of Placement: <span class="date-time"></span></p>
+                <p class="location">Location: ${item.country}</p>
+                <p class="sponsored"> Sponsored by : ${item.sponsored}</p>
+                <button class="like" type="button">
+                    <img src="/asset/imgs/heart.svg" alt="" class="heart">
+                    <p class="text">50</p>
+                </button>
+                </artile>
+                </div>`
+            });
+            displayItem = displayItem.join("");
+    
+    
+            sectionCenter.innerHTML = displayItem;
         }
     })
 
